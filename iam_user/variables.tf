b080513b0c992b40ca786148aad982cd4a6d4ecd
wozoopa <user@localhost>
2018-08-08 23:30:52 -0500

    Added another missing variable.

--- iam_user/variables.tf
+++ iam_user/variables.tf
@@ -1,3 +1,4 @@
+variable "username" { }
 variable "temp_pass" { default = "Ch@nGeME#9403" }
 variable "no_key_no_console" { default = false }
 variable "enable_access_key_no_console" { default = false }

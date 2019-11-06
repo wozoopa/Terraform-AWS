707d4efa940ba55ad805850f0acea7bbdbbb4263
Ernest Gulik <Ernest>
2019-11-06 14:59:18 +0000

    Added dhcp options to vpc module.

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,6 +2,13 @@
 # This module creates all resources necessary for a VPC
 #--------------------------------------------------------------
 
+resource "aws_vpc_dhcp_options" "internal_dhcp" {
+    domain_name = "${var.domain}"
+
+    tags {
+        Name = "${var.domain}-DHCP"
+    }
+}
 
 resource "aws_vpc" "vpc" {
   cidr_block           = "${var.cidr}"
@@ -12,3 +19,7 @@ resource "aws_vpc" "vpc" {
   lifecycle { create_before_destroy = true }
 }
 
+resource "aws_vpc_dhcp_options_association" "internal_domain" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    dhcp_options_id = "${aws_vpc_dhcp_options.internal_dhcp.id}"
+}

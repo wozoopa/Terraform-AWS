6a514cc03dcb61531c09ca0c23d3f36e6340cd73
egulik <8609503+wozoopa@users.noreply.github.com>
2020-04-19 21:08:54 -0500

    Updated variables in private_subnet module.

--- private_subnet/variables.tf
+++ private_subnet/variables.tf
@@ -2,7 +2,7 @@ variable "name"                  { default = "private"}
 variable "vpc_id"                { }
 variable "cidrs"                 { }
 variable "azs"                   { }
-variable "nat_gateway_ids"       { default = "" }
-variable "use_ec2_nat"           { default = "" }
-variable "use_nat_gateway"       { default = "" }
+variable "nat_gateway_ids"       { }
+variable "use_ec2_nat"           { }
+variable "use_nat_gateway"       { }
 variable "nat_instance_ids"      { default = "" }
